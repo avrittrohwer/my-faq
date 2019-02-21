ad3eb8c44855fd1193dd706eb264c481b8c009b0
Avritt Rohwer <avritt.rohwer@gmail.com>
2019-02-21 12:54:26 -0500

    made it simple to publish to gh-pages, added things to serve from /my-faq

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/my-faq">
     <App />
   </BrowserRouter>,
   document.getElementById("root")
